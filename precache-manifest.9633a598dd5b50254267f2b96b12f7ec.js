ac6bc3b06ebf27e38f9541ad2e83e2f877b6e5d1
Michael Dean <mjdean84@gmail.com>
2020-02-13 15:09:54 -0500

    Updates

---
+++ precache-manifest.9633a598dd5b50254267f2b96b12f7ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c767155e65f783b0ee864e2e773bf3",
+    "url": "/bellstand-app/index.html"
+  },
+  {
+    "revision": "e679f95de63e4e172f9a",
+    "url": "/bellstand-app/static/css/main.2531daa2.chunk.css"
+  },
+  {
+    "revision": "b103769da9b27076fc98",
+    "url": "/bellstand-app/static/js/2.3a6dabec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bellstand-app/static/js/2.3a6dabec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e679f95de63e4e172f9a",
+    "url": "/bellstand-app/static/js/main.8a04b260.chunk.js"
+  },
+  {
+    "revision": "4d8b5c827dbbe57c4e93",
+    "url": "/bellstand-app/static/js/runtime-main.52cd42f5.js"
+  }
+]);
